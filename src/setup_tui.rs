78c27a7067e4a350f6374fb4a6f6dc5aee4f2d0f
dumko2001 <dumko.raj@gmail.com>
2026-03-14 17:33:34 +0530

    chore: save progress before multi-account

--- src/setup_tui.rs
+++ src/setup_tui.rs
@@ -30,6 +30,7 @@ use ratatui::{
     widgets::{Block, Borders, List, ListItem, ListState, Paragraph},
     DefaultTerminal,
 };
+use ratatui::prelude::Stylize;
 use std::io::stdout;
 
 /// An item in the multi-select list.
